214993ec839d4edf6eba312f4f424700f9326c41
Kenneth Halim <kanisiuskenneth@gmail.com>
2018-11-20 20:45:00 +0700

    Add ListenerImplementation class

---
+++ src/main/java/com/rplsd/scheduler/ZadwalListenerImpl.java
@@ -0,0 +1,13 @@
+package com.rplsd.scheduler;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+public class ZadwalListenerImpl extends ZadwalBaseListener {
+    @Override public void enterDefineClassroom(ZadwalParser.DefineClassroomContext ctx) {
+        System.out.println("rule entered: " + ctx);
+    }
+    @Override public void enterDefineLecturer(ZadwalParser.DefineLecturerContext ctx) {
+        System.out.println("rule entered: " + ctx.getText());
+
+    }
+}
