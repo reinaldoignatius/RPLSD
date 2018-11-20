4feae9998b04b3b0b65ff6213c462c6e237ded8b
andika <dika.kusuma23@gmail.com>
2018-11-20 18:40:29 +0700

    Fix constructor

--- src/main/java/com/rplsd/scheduler/App.java
+++ src/main/java/com/rplsd/scheduler/App.java
@@ -1,6 +1,28 @@
 package com.rplsd.scheduler;
+
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        ArrayList<ClassRoom> classRooms = new ArrayList<>();
+        ArrayList<ClassRequirement> classRequirements = new ArrayList<>();
+        ArrayList<Lecturer> lecturers = new ArrayList<>();
+        ScheduleConstraint scheduleConstraint = new ScheduleConstraint(3);
+        SchedulePreference schedulePreference = new SchedulePreference();
+        Scheduler scheduler = new Scheduler(
+                classRooms, classRequirements, lecturers, scheduleConstraint, schedulePreference);
+
+        scheduler.addClassRoom(new ClassRoom("7602", 100, new ArrayList<String>(){{
+            add("ac");
+            add("ac2");
+        }}));
+
+        ArrayList<ArrayList<Boolean>> classRoomAvailability = scheduler.getClassRoomsAvailability().get("7602");
+        for (ArrayList<Boolean> day: classRoomAvailability) {
+            for (Boolean time: day) {
+                System.out.println(time);
+            }
+        }
     }
 }
