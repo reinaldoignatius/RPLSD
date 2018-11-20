7afc39b77b04d3e1eacc51e37001b3c56fc05492
Kenneth Halim <kanisiuskenneth@gmail.com>
2018-11-20 16:20:51 +0700

    Edit gradle setting and add sample grammar

---
+++ generated-src/main/java/testListener.java
@@ -0,0 +1,19 @@
+// Generated from test.g by ANTLR 4.7.1
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+
+/**
+ * This interface defines a complete listener for a parse tree produced by
+ * {@link testParser}.
+ */
+public interface testListener extends ParseTreeListener {
+	/**
+	 * Enter a parse tree produced by {@link testParser#r}.
+	 * @param ctx the parse tree
+	 */
+	void enterR(testParser.RContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link testParser#r}.
+	 * @param ctx the parse tree
+	 */
+	void exitR(testParser.RContext ctx);
+}
