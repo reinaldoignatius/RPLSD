753375fe1d142a4f1aed5b5653e4f5a63f865d1a
Kenneth Halim <kanisiuskenneth@gmail.com>
2018-11-21 19:21:08 +0700

    Implement listener for classroom and lecturer parser

--- src/main/java/com/rplsd/zadwal/App.java
+++ src/main/java/com/rplsd/zadwal/App.java
@@ -9,6 +9,9 @@ public class App
 //        Scanner scanner = new Scanner(System.in);
 //        scanner.nextLine();
         ZadwalDriver driver = new ZadwalDriver();
-        driver.start("classroom R7602 50 [projector,ac];");
+        driver.start("CLASSROOM R7602 50 [projector,ac];"+
+                "CLASSROOM R7606 50 [projector, ac, internet];"+
+                "Lecturer YA [11,12,13,14,21,22,23,24,31,32,33,34];"
+                );
     }
 }
